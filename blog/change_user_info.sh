aee5fa6ef1148c91b61fd5f07f2c777c7f63d0ad
imoyao <immoyao@gmail.com>
2019-11-06 21:12:19 +0800

    add change user info shell

---
+++ blog/change_user_info.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+an="$GIT_AUTHOR_NAME"
+am="$GIT_AUTHOR_EMAIL"
+cn="$GIT_COMMITTER_NAME"
+cm="$GIT_COMMITTER_EMAIL"
+if [ "$GIT_COMMITTER_EMAIL" = "imsantu.ma@gmail.com" ]
+then
+    cn="imoyao"
+    cm="immoyao@gmail.com"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "imsantu.ma@gmail.com" ]
+then
+    an="imoyao"
+    am="immoyao@gmail.com"
+fi
+    export GIT_AUTHOR_NAME="$an"
+    export GIT_AUTHOR_EMAIL="$am"
+    export GIT_COMMITTER_NAME="$cn"
+    export GIT_COMMITTER_EMAIL="$cm"
