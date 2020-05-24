299039e98721f16eda69c3fcc186f63e5f2056cd
imoyao <immoyao@gmail.com>
2020-05-25 00:41:35 +0800

    no subdir

--- change_user_info.sh
+++ change_user_info.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 # https://segmentfault.com/q/1010000006999861
-# https://stackoverflow.com/questions/750172/how-to-change-the-author-and-committer-name-and-e-mail-of-multiple-commits-in-gi
-
-git filter-branch --force --env-filter '
-    if [ "$GIT_COMMITTER_NAME" = "masantu" ];
-    then
-        GIT_COMMITTER_NAME="imoyao";
-        GIT_COMMITTER_EMAIL="immoyao@gmail.com";
-        GIT_AUTHOR_NAME="imoyao";
-        GIT_AUTHOR_EMAIL="immoyao@gmail.com";
-    fi' -- --all
+git filter-branch --env-filter '
+an="$GIT_AUTHOR_NAME"
+am="$GIT_AUTHOR_EMAIL"
+cn="$GIT_COMMITTER_NAME"
+cm="$GIT_COMMITTER_EMAIL"
+if [ "$GIT_COMMITTER_EMAIL" = "imsantu.ma@gmail.com" ]
+then
+    cn="imoyao"
+    cm="immoyao@gmail.com"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "imsantu.ma@gmail.com" ]
+then
+    an="imoyao"
+    am="immoyao@gmail.com"
+fi
+    export GIT_AUTHOR_NAME="$an"
+    export GIT_AUTHOR_EMAIL="$am"
+    export GIT_COMMITTER_NAME="$cn"
+    export GIT_COMMITTER_EMAIL="$cm"
